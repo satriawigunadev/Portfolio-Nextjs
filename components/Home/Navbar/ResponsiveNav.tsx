1649b2d693311c64e8aa1b969b6a8e9c7d81cda6
satriawigunadev <satria.ctwy@gmail.com>
2025-11-08 00:06:35 +0700

    commit 2
    commit 2
    
    Signed-off-by: satriawigunadev <satria.ctwy@gmail.com>

---
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+
+function ResponsiveNav() {
+  return (
+    <div>
+      <Nav />
+      <MobileNav />
+    </div>
+  );
+}
+
+export default ResponsiveNav;
